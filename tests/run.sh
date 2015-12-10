30d04cac3434b800aa588fa54693c6ae50c03297
franklin <franklin@weareinteractive.com>
2015-12-10 18:39:35 +0100

    chor: updates travis tests

---
+++ tests/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# define variables
+ROLE_NAME=franklinkim.users
+
+# install dependencies
+# ansible-galaxy install franklinkim.users
+
+# create role symnlink
+ln -s $(pwd) /usr/share/ansible/roles/$ROLE_NAME
+
+echo 'running playbook'
+ansible-playbook --syntax-check -i inventory $(pwd)/tests/main.yml
+ansible-playbook -vvvv -i 'localhost,' -c local $(pwd)/tests/main.yml
