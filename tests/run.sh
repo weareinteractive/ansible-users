a58961d075d041d495051322b92489a7918cdc46
franklin <franklin@weareinteractive.com>
2016-04-14 11:21:39 +0200

    docs: add echo output

--- tests/run.sh
+++ tests/run.sh
@@ -4,12 +4,16 @@
 ROLE_NAME=franklinkim.users
 
 # install dependencies
+echo '* installing dependencies'
 apt-get update
 apt-get install -y openssh-client
 #ansible-galaxy install franklinkim.openssl
 
-# create role symnlink
+# create role symlink
+echo '* creating symlink'
 ln -s $(pwd) /usr/share/ansible/roles/$ROLE_NAME
 
+# run tests
 echo 'running playbook'
+ansible-playbook --syntax-check -i 'localhost,' -c local $(pwd)/tests/main.yml
 ansible-playbook -vvvv -i 'localhost,' -c local $(pwd)/tests/main.yml
